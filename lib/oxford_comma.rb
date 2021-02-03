72929a7b109a9965856ee2a9d909c44075074b0b
Lupita <lupitarivera8899@gmail.com>
2021-02-03 17:50:17 -0600

    commit tree 85a3089c2d773bbf1d789926f5239449d45c8c8b

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
 
-end
+# require "pry"
+
+def oxford_comma(array)
+    if array.length == 1
+       return array.join
+     elsif array.length == 2
+        return array.join(" and ")
+     else
+        lastStr = array.pop     #last string and save variable
+        array.join(", ") << ", and #{lastStr}"  #join array with comma and shovel the , and last string back again
+     end
+  end
+  
